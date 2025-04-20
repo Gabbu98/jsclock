02e796942b76375a33adaaf2ed767b66eae1927f
Gabbu98 <35544491+Gabbu98@users.noreply.github.com>
2025-04-20 16:58:57 +0000

    separated code

---
+++ index.js
@@ -0,0 +1,15 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+
+    h = checkTime(h);
+    m = checkTime(m);
+    document.getElementById('txt').innerHTML =  h + ":" + m;
+    setTimeout(startTime, 1000); 
+}
+
+function checkTime(i) {
+     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
